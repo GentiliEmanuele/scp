2bf1a24c034f4aa5b9d63095f5b36350182f0c8b
Francesco Donnini <donnini.francesco00@gmail.com>
2025-01-14 16:28:21 +0100

    git removed some dead code

--- spmv_openmp.h
+++ spmv_openmp.h
@@ -7,6 +7,5 @@ int d_spmv_csr_par(double *res, struct csr *csr, double *v, int n);
 int i_spmv_csr_par(int *res, struct csr *csr, int *v, int n);
 int d_spmv_hll_par(double *res, struct hll *hll, double *v, int n);
 int i_spmv_hll_par(int *res, struct hll *hll, int *v, int n);
-int d_spmv_hll_par2(double *res, struct hll *hll, double *v, int n);
 
 #endif
