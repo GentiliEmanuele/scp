f844fa81ae31e75b5003ac5c1b66031441b1508d
Francesco Donnini <donnini.francesco00@gmail.com>
2025-01-16 14:50:05 +0100

    reworked main.py
    Co-authored-by: Francesco Donnini <francescodonnini@users.noreply.github.com>

--- temp_main.c
+++ temp_main.c
@@ -1,17 +1,10 @@
+#include "csr.h"
 #include "hll.h"
 #include "utils.h"
 #include "vec.h"
 #include "spmv_openmp.h"
 #include <stdio.h>
 #include <string.h>
-
-const char *DATA_DIR = "data";
-
-const char *TEST_DIR = "test/output";
-
-char *paths[] = {
-    "1138_bus.mtx", "3x3.mtx", "4x4.mtx", "af23560.mtx", "cage4.mtx", "mbeacxc.mtx", "sym3.mtx", "sym4.mtx"
-};
 
 int read_vector(double *vector, int n, const char *path) {
     FILE *f = fopen(path, "r");
@@ -58,6 +51,39 @@ int test_hll(const char *file, const char *vfile, const char *rfile, int hack_si
     return 0;
 }
 
+int test_csr(const char *file, const char *vfile, const char *rfile) {
+    struct MatrixMarket mm;
+    if (read_mtx(file, &mm)) {
+        return 0;
+    }
+
+    struct csr sm;
+    if (csr_init(&sm, &mm)) {
+        mtx_cleanup(&mm);
+        return 1;
+    }
+
+    int m = sm.num_rows;
+    int n = sm.num_cols;
+    double *v = d_zeros(m);
+    if (read_vector(v, m, vfile)) {
+        mtx_cleanup(&mm);
+        return 0;
+    }
+    double *p = d_zeros(m);
+    if (d_spmv_csr_par(p, &sm, v, n)) {
+        mtx_cleanup(&mm);
+        csr_cleanup(&sm);
+        return 1;
+    }
+    double *r = d_zeros(m);
+    read_vector(r, m, rfile);
+    int err = d_veceq(r, p, m, 1e-6) ? 0 : 1;
+    mtx_cleanup(&mm);
+    csr_cleanup(&sm);
+    return 0;
+}
+
 int main(int argc, char **argv) {
     if (--argc != 1) {
         printf("see usage: program matrices_file\n");
@@ -78,7 +104,7 @@ int main(int argc, char **argv) {
         sprintf(mfile, "data/%s", line);
         sprintf(rfile, "test/output/%s.result", line);
         sprintf(vfile, "test/output/%s.vector", line);
-        if (test_hll(mfile, vfile, rfile, 32)) {
+        if (test_csr(mfile, vfile, rfile)) {
             printf("test failed for %s\n", line);
             return 1;
         }
--- test/main.py
+++ test/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from io import StringIO
-from scipy.io import mmread
+from scipy.io import mminfo, mmread
 from scipy.sparse import coo_matrix
 import os
 import sys
@@ -8,15 +8,20 @@ import sys
 def mm_name(mm_path):
     return os.path.basename(mm_path)
 
-def test(mm_path):
+def test(mm_path, output_dir):
     with open(mm_path) as fp:
         try:
-            mm = mmread(StringIO(fp.read()))
+            text = fp.read()
+            (_, _, _, _, field, _) = mminfo(StringIO(text))
+            if field == 'complex':
+                print(f'({mm_name(mm_path)}) complex matrix not supported')
+                return
+            mm = mmread(StringIO(text))
             v = np.random.rand(mm.shape[1], 1)
             result = mm.dot(v)
-            vpath = os.path.join(OUTPUT_DIR, f'{mm_name(mm_path)}.vector')
+            vpath = os.path.join(output_dir, f'{mm_name(mm_path)}.vector')
             np.savetxt(vpath, v, delimiter='\n', fmt='%f')
-            rpath = os.path.join(OUTPUT_DIR, f'{mm_name(mm_path)}.result')
+            rpath = os.path.join(output_dir, f'{mm_name(mm_path)}.result')
             np.savetxt(rpath, result, delimiter='\n', fmt='%f')
         except ValueError as e:
             print(f'(matrix={mm_name(mm_path)}) {e}')
@@ -24,7 +29,13 @@ def test(mm_path):
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print("see usage: program matrices_directory output_directory")
-    MATRICES = sys.argv[1]
-    OUTPUT_DIR = sys.argv[2]
-    for mm_path in list(filter(lambda x: x.endswith('.mtx'), os.listdir(MATRICES))):
-        test(os.path.join(MATRICES, mm_path))
+    matrices_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    try:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        for mm_path in list(filter(lambda x: x.endswith('.mtx'), os.listdir(matrices_dir))):
+            test(os.path.join(matrices_dir, mm_path), output_dir)
+    except OSError as e:
+        print(f'cannot create directory {output_dir}, got error {e}')
