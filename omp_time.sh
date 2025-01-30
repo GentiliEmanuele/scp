44d8818caedd900f43f67fcde19c20851e56fd84
GentiliEmanuele <egentili2000@gmail.com>
2025-01-30 17:52:20 +0100

    modified the bash file for iterate on num_threads
    Co-authored-by: Francesco Donnini <francescodonnini@users.noreply.github.com>

--- omp_time.sh
+++ omp_time.sh
@@ -1,13 +1,13 @@
-if [ "$#" -ne 4 ]; then
-    echo "usage: $0 <file> (csr|hll) <output> <max number of threads>"
+if [ "$#" -ne 3 ]; then
+    echo "usage: $0 <file> (csr|hll) <max number of threads>"
     exit 1
 fi
 if ["$2" -ne "csr" ] && ["$2" -ne "hll"]; then
     echo "usage: $0 <file> (csr|hll)"
     exit 1
 fi
-for i in $(seq 1 $4);
+for i in $(seq 1 $3);
 do
     echo "iteration number: $i"
-    ./build/omp_time_"$2".exe "$1" $i 10 "$3"
+    ./build/omp_time_"$2".exe "$1" $i 10 "$2_$i"
 done
