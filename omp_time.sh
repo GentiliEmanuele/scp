88dd9331700e8aec1070c55f0b72ff5b1af824f3
Francesco Donnini <donnini.francesco00@gmail.com>
2025-02-07 12:11:40 +0100

    omp_time wrong initial number of thread

--- omp_time.sh
+++ omp_time.sh
@@ -6,7 +6,7 @@ if ["$2" -ne "csr" ] && ["$2" -ne "hll"]; then
     echo "usage: $0 <file> (csr|hll)"
     exit 1
 fi
-for i in $(seq 22 $3);
+for i in $(seq 1 $3);
 do
     echo "iteration number: $i"
     ./build/omp_time_"$2".exe "$1" $i 100 "$4/$2_$i"
