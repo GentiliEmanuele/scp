9a8dbee93987ab5f25f900731ca1cfee95952c73
Francesco Donnini <donnini.francesco00@gmail.com>
2025-02-10 15:43:00 +0100

    risultati hll dynamic

--- omp_time_hll.sh
+++ omp_time_hll.sh
@@ -2,7 +2,7 @@ if [ "$#" -ne 3 ]; then
     echo "usage: $0 <file> <max number of threads> <output_dir>"
     exit 1
 fi
-for i in $(seq 21 $2);
+for i in $(seq 1 $2);
 do
     echo "iteration number: $i"
     ./build/omp_time_hll.exe "$1" 32 $i 100 "$3/hll_$i"
