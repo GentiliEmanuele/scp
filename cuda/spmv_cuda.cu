fb7c68bc118d9f900b16ddacfff704874bbaf92e
Francesco Donnini <donnini.francesco00@gmail.com>
2025-02-21 21:11:47 +0100

    no test

--- cuda/spmv_cuda.cu
+++ cuda/spmv_cuda.cu
@@ -4,19 +4,9 @@
 
 __global__ void cuda_spmv_hll(double *res, int hack_size, int hacks_num, double *data, int *offsets, int *col_index,  int *max_nzr, double *v, int n) {
     int h = blockDim.x * blockIdx.x + threadIdx.x;
-    if (h < hacks_num - 1) {
-    	int rows = hack_size;
-    	for (int r = 0; r < hack_size; ++r) {
-        	double sum = 0.0;
-        	for (int j = 0; j < max_nzr[h]; ++j) {
-            		int k = offsets[h] + r * max_nzr[h] + j;
-            		sum += data[k] * v[col_index[k]];
-        	}
-        	res[rows * h + r] = sum;
-    	}
-    } else if (h == hacks_num - 1) {
-        int rows = n % hack_size;
-        for (int r = 0; r < n % hack_size; ++r) {
+    if (h < hacks_num) {
+    	int rows = num_of_rows(0, hack_size, hacks_num, n);
+    	for (int r = 0; r < num_of_rows(h, hack_size, hacks_num, n); ++r) {
         	double sum = 0.0;
         	for (int j = 0; j < max_nzr[h]; ++j) {
             		int k = offsets[h] + r * max_nzr[h] + j;
@@ -37,4 +27,3 @@ __global__ void cuda_spmv_csr(double *res, int *row_pointer, double *data, int *
         res[i] = sum;
     }
 }
-
