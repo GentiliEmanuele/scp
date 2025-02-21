d551a4a057c0b235bb56c21b12745fc6d499df63
Francesco Donnini <donnini.francesco00@gmail.com>
2025-02-21 16:46:11 +0100

    fixed bash file

--- omp/omp_main_time_hll.c
+++ omp/omp_main_time_hll.c
@@ -8,11 +8,11 @@ int main(int argc, char **argv) {
         printf("usage:\nprogram matrices_list num_runs num_threads hack_size result_file_path\n");
         return 1;
     }
-    char *out_path = argv[--argc];
-    int num_iterations = atoi(argv[--argc]);
-    int num_thread = atoi(argv[--argc]);
-    int hack_size = atoi(argv[--argc]);
-    char *path = argv[--argc];
+    char *out_path = argv[5];
+    int num_iterations = atoi(argv[4]);
+    int num_thread = atoi(argv[3]);
+    int hack_size = atoi(argv[2]);
+    char *path = argv[1];
     read_and_measure_hll(path, hack_size, num_iterations, num_thread, out_path);
     return 0;
 }
--- omp_time.sh
+++ omp_time.sh
@@ -11,13 +11,13 @@ if [ "$2" == "csr" ]; then
 else
     max_num_threads=$4
 fi
-for i in $(seq 1 $max_num_threads);
+for i in $(seq 10 $max_num_threads);
 do
     export OMP_NUM_THREADS=$i
     echo "iteration number $i with #threads=$OMP_NUM_THREADS"
     if [ "$2" == "csr" ]; then
         ./build/omp_time_"$2".exe "$1" $i 100 "$4/$2_$i"
     else
-        ./build/omp_time_"$2".exe "$1" $3 $i 100 "$4/$2_$i"
+        ./build/omp_time_"$2".exe "$1" $3 $i 100 "$5/$2_$i"
     fi
 done
