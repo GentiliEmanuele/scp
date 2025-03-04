ebeebb7f6482d97c05e39dd53663f41a6d48abc4
GentiliEmanuele <egentili2000@gmail.com>
2025-03-04 21:19:55 +0100

    Fix a bug

--- cuda/spmv_cuda.cu
+++ cuda/spmv_cuda.cu
@@ -30,7 +30,7 @@ __global__ void cuda_spmv_hll_v2(double *res, int hack_size, int hacks_num, doub
         int row_start = row * max_nzr[hack] + offsets[hack];
         int row_end = (row + 1) * max_nzr[hack] + offsets[hack];
         if (get_hack(row + 1, n, hacks_num) != hack) {
-            row_end = offsets[get_hack(row + 1, n, hack_num)]
+            row_end = offsets[get_hack(row + 1, n, hacks_num)];
         }
         for (int element = row_start + lane; element < row_end; element += 32) {
             sum += data[element] * v[col_index[element]];
